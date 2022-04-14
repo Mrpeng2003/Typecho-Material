56bf0935dc0772860dff05f4ffc930e4f93e6a21
Mrpeng2003 <94415792+Mrpeng2003@users.noreply.github.com>
2022-04-14 10:56:34 +0800

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+	<meta charset="UTF-8" />	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />		
+	<meta http-equiv="Cache-Control" content="no-transform" />
+	<meta http-equiv="Cache-Control" content="no-siteapp" />
+	<meta name="viewport" width="device-width", initial-scale="1.0" />
+	<title>错误 - 茶番屋</title>
+	<link href="https://cdn.jsdelivr.net/gh/Mrpeng2003/Nistudio/crazyp/img/pugin/404.css" type="text/css" rel="stylesheet">
+	<style>
+        .container {
+            width: 60%;
+            margin: 10% auto 0;
+            background-color: #f0f0f0;
+            padding: 2% 5%;
+            border-radius: 10px}
+        ul {padding-left: 20px;}
+            ul li {line-height: 2.3}
+        a {color: #20a53a}
+    </style>
+</head>
+<!--/************************************************************
+ *																*
+ * 						     Mr.Peng							*
+ *                    acg23.xyz crazypeng.xyz					*
+ *       		        一位爱分享的OTAKU		            	*
+ * 																*
+**************************************************************-->
+<body>
+<div class="not-found-page">
+	<div class="content">
+		<img src="https://cdn.jsdelivr.net/gh/Mrpeng2003/Nistudio/404/err.png" class="top-img">
+		<div class="info"><h2>抱歉！页面无法访问……</h2>
+			<ul>
+			<div class="container">
+			<li> 请检查地址是否完整或存在多余字符</li>
+			<li>可能页面已被删除</li>
+			</ul>
+			<p class="state">你可以返回<a href="#" onClick="javascript:history.back(-1);">上一页</a>，本页面将在10秒后跳转至网站首页</p>
+			<script type="text/javascript">
+				setTimeout(function () {window.location.href = "/";}, 11000);
+			</script>
+			<div class="search"></div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,351 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+
+define("MATERIAL_VERSION", "3.4.0");
+
+require_once("lib/tools.php");
+require_once("lib/UACheck.php");
+require_once("lib/pangu.php");
+require_once("lib/Spyc.php");
+require_once("lib/ThemeOptionRender.php");
+require_once("lib/ThemeOption.php");
+
+if (!defined('__TYPECHO_DEBUG__') || __TYPECHO_DEBUG__ == 0) {
+    error_reporting(0);
+}
+
+if (isset($this)) {
+    global $t;
+    $t = $this;
+}
+
+if (isset($_GET["mod"]) && isset($this) && $this->is('index')) {
+    if ($_GET["mod"] === "search-xml") {
+        $this->need("page-search.php");
+        exit;
+    }
+    if ($_GET["mod"] === "export") {
+        if ($_GET['type'] === 'comments') {
+            if (Typecho_Widget::widget('Widget_User')->pass('administrator', true)) {
+                header("Content-Type: text/xml");
+                header('Content-Disposition: attachment; filename="'.Typecho_Widget::widget('Widget_Options')->title.'-comments-wxr-'.gmdate('Y-m-d').'.xml"');
+                $tool = new Comment_Export();
+                $db = Typecho_Db::get();
+                $query = $db->select('*')->from('table.contents')->where('type = ?', 'post')->orWhere('type = ?', 'page');
+                $result = $db->fetchAll($query);
+                foreach ($result as $item) {
+                    $tool->addBlock($item);
+                }
+                $comment_query = $db->select('*')->from('table.comments');
+                $comments = $db->fetchAll($comment_query);
+                foreach ($comments as $comment) {
+                    if ($comment['status'] !== 'spam') {
+                        $tool->addComment($comment);
+                    }
+                }
+                echo $tool->getResult();
+                exit;
+            }
+        }
+    }
+}
+
+/**
+ * JavaScript 载入
+ * @param string name
+ * @param string uri
+ */
+function jsLsload($name, $uri)
+{
+    if (in_array("LocalStorage", getThemeOptions("switch"))) {
+        $options = Helper::options();
+        $identifier = $name . $uri . filemtime($options->themeFile(getTheme(), $uri)) . MATERIAL_VERSION;
+        $hash = md5($identifier);
+        echo '<script>lsloader.load("' . $name . '","' . getThemeFile($uri) . '?' . $hash . '", true)</script>';
+    } else {
+        echo '<script src="'.getThemeFile($uri).'"></script>';
+    }
+}
+
+/**
+ * CSS 载入
+ * @param string name
+ * @param string uri
+ */
+function cssLsload($name, $uri)
+{
+    if (in_array("LocalStorage", getThemeOptions("switch"))) {
+        $options = Helper::options();
+        $identifier = $name . $uri . filemtime($options->themeFile(getTheme(), $uri)) . MATERIAL_VERSION;
+        $hash = md5($identifier);
+        echo '<style id="' . $name . '"></style>';
+        echo '<script>if(typeof window.lsLoadCSSMaxNums === "undefined")window.lsLoadCSSMaxNums = 0;window.lsLoadCSSMaxNums++;lsloader.load("' . $name . '","' . getThemeFile($uri) . '?' . $hash . '",function(){if(typeof window.lsLoadCSSNums === "undefined")window.lsLoadCSSNums = 0;window.lsLoadCSSNums++;if(window.lsLoadCSSNums == window.lsLoadCSSMaxNums)document.documentElement.style.display="";}, false)</script>';
+    } else {
+        echo '<link href="'.getThemeFile($uri).'" rel="stylesheet" type="text/css" />';
+    }
+}
+
+function getScriptType()
+{
+    if (in_array("LocalStorage", getThemeOptions("switch"))) {
+        echo 'text/ls-javascript';
+    } else {
+        echo 'text/javascript';
+    }
+}
+
+function getBackgroundLazyload($url)
+{
+    if (in_array("LazyloadIndex", getThemeOptions("switch"))) {
+        echo 'data-original="' . $url . '"';
+    } else {
+        echo 'style="background-image: url(\'' . $url . '\');"';
+    }
+}
+
+function getThemeFile($uri, $print = false)
+{
+    $options = Helper::options();
+    if (getThemeOptions("CDNType") == 1) {
+        $url = "https://cdn.jsdelivr.net/gh/idawnlight/typecho-theme-material@" . MATERIAL_VERSION . "/" . $uri;
+    } else if (getThemeOptions("CDNType") == 3) {
+        $url = "https://shadow.elemecdn.com/gh/idawnlight/typecho-theme-material@" . MATERIAL_VERSION . "/" . $uri;
+    } else if (getThemeOptions("CDNType") == 2) {
+        $url = getThemeOptions("CDNURL") . "/" . $uri;
+    } else {
+        $site = substr($options->siteUrl, 0, strlen($options->siteUrl) - 1);
+        $url = $site . __TYPECHO_THEME_DIR__ . "/" . getTheme() . "/" . $uri;
+    }
+    if ($print) echo $url;
+    return $url;
+}
+
+/**
+ * 获取当前使用的主题名称
+ * @return string theme name
+ */
+function getTheme()
+{
+    static $themeName = NULL;
+    if ($themeName === NULL) {
+        $db = Typecho_Db::get();
+        $query = $db->select('value')->from('table.options')->where('name = ?', 'theme');
+        $result = $db->fetchAll($query);
+        $themeName = $result[0]["value"];
+    }
+    return $themeName;
+}
+
+/**
+ * 获取当前的主题设置
+ * @param null $setting
+ * @param bool $print
+ * @param null $default
+ * @return mixed setting value
+ * @throws Typecho_Db_Exception
+ */
+function getThemeOptions($setting = null, $print = false, $default = null)
+{
+    static $themeOptions = NULL;
+    if ($themeOptions === NULL) {
+        $db = Typecho_Db::get();
+        $query = $db->select('value')->from('table.options')->where('name = ?', 'theme:' . getTheme());
+        $result = $db->fetchAll($query);
+        $themeOptions = unserialize($result[0]["value"]);
+    }
+    $result = ($setting === NULL) ? $themeOptions : (isset($themeOptions[$setting]) ? $themeOptions[$setting] : $default);
+    if (is_string($result) && $print) echo $result;
+    return $result;
+}
+
+function themeInit($archive)
+{
+    if (($archive->is('post') || $archive->is('page')) && in_array("Lazyload", getThemeOptions("switch"))) {
+        $archive->content = preg_replace('#<img(.*?) src="(.*?)" (.*?)>#',
+            '<img$1 data-original="$2" class="lazy" $3>', $archive->content);
+    }
+    $options = Helper::options();
+    if ($options->version === "1.1/17.10.30") {
+        $archive->content = preg_replace('#<li><p>(.*?)</p>(.*?)</li>#',
+            '<li>$1$2</li>', $archive->content);
+    }
+    $options->commentsAntiSpam = false;
+}
+
+/**
+ * 获取二维码
+ * @param string permalink
+ * @return string url
+ */
+function getQRCode($permalink) {
+    $qrcode = getThemeOptions("qrcode");
+    if ($qrcode === NULL) $qrcode = 1;
+    $src = "";
+    switch ($qrcode) {
+        case 0:
+            $src = "https://img.crazypeng.xyz/qrcode/qr.php?w=80&t=$permalink";
+            break;
+        case 1:
+            $src = "https://img.crazypeng.xyz/qrcode/qr.php?w=80&t=$permalink";
+            break;
+        case 2:
+            $src = "https://chart.googleapis.com/chart?chs=200x200&cht=qr&chld=H|1&chl=$permalink";
+            break;
+        case 3:
+            $src = "https://img.crazypeng.xyz/qrcode/qr.php?w=80&t=$permalink";
+            break;
+    }
+    echo $src;
+    return $src;
+}
+
+/**
+ * 文章缩略图
+ * @param Typecho_Widget $widget
+ * @return string image url
+ */
+function showThumbnail($widget)
+{
+    if ($widget->fields->picUrl){
+        return $widget->fields->picUrl;
+    }
+
+    //If article no include picture, display random default picture
+    $result = randomThumbnail();
+
+    $attach = $widget->attachments(1)->attachment;
+    if (isset($attach) && $attach->isImage) {
+        $result = $attach->url;
+    }
+
+    if (getThemeOptions("FetchFirstImageRegex", false, '0') == '0') {
+        global $t;
+        if (in_array("Lazyload", getThemeOptions("switch")) && method_exists($t,'is') && !$t->is('index')) {
+            if (preg_match_all('/\<img.*?data-original\=\"(.*?)\"[^>]*>/i', $widget->content, $thumbUrl)) {
+                $result = $thumbUrl[1][0];
+            }
+        } else {
+            if (preg_match_all('/\<img.*?src=\"(.*?)\"[^>]*>/i', $widget->content, $thumbUrl)) {
+                $result = $thumbUrl[1][0];
+            }
+        }
+    }
+
+    return $result;
+}
+
+/**
+ * 随机缩略图
+ * @return string image url
+ * @throws Typecho_Db_Exception
+ */
+function randomThumbnail()
+{
+    $rand = rand(1, getThemeOptions('RandomPicAmnt'));
+
+    return getThemeFile('img/random/material-' . $rand . '.png');
+}
+
+/**
+ * Console Copyright
+ */
+function copyright()
+{
+    echo '<script>console.log("\n %c © Material ' . MATERIAL_VERSION . ' | https://github.com/idawnlight/typecho-theme-material %c \n","color:#455a64;background:#e0e0e0;padding:5px 0;border-top-left-radius:5px;border-bottom-left-radius:5px;","color:#455a64;background:#e0e0e0;padding:5px 0;border-top-right-radius:5px;border-bottom-right-radius:5px;")</script>';
+}
+
+/**
+ * Multi-language support
+ * @param string English 英文翻译
+ * @param string Chinese 中文翻译
+ * @param int languageIs 语言设置
+ * @return string 对应翻译
+ */
+function tranMsg($eng, $chs, $l)
+{
+    return ($l == "0") ? $eng : $chs ;
+}
+
+/**
+ * i18n support
+ * @param string expression
+ * @return string translation
+ */
+function lang($expression, $display = true)
+{
+    static $lang = NULL;
+    $language = (getThemeOptions("language") !== NULL) ? getThemeOptions("language") : "zh-CN";
+    if ($lang === NULL) $lang = Spyc::YAMLLoad(Helper::options()->themeFile(getTheme(), "languages/".$language.".yml"));
+    $now = $lang;
+    foreach (explode(".", $expression) as $exp) {
+        if (isset($now[$exp])) {
+            if (!is_array($now[$exp])) {
+                if ($display) echo $now[$exp];
+                return $now[$exp];
+            }
+            $now = $now[$exp];
+        } else {
+            return false;
+        }
+    }
+}
+
+/**
+ * Pangu.PHP
+ * @param string html_source
+ * @return string 处理完的 html_source
+ */
+function pangu($html_source)
+{
+    $chunks = preg_split('/(<!--<nopangu>-->.*?<!--<\/nopangu>-->|<nopangu>.*?<\/nopangu>|<pre.*?\/pre>|<textarea.*?\/textarea>|<code.*?\/code>)/msi', $html_source, -1, PREG_SPLIT_DELIM_CAPTURE);
+    $result = '';
+    foreach ($chunks as $c) {
+        if (strtolower(substr($c, 0, 16)) == '<!--<nopangu>-->') {
+            $c = substr($c, 16, strlen($c) - 16 - 17);
+            $result .= $c;
+            continue;
+        } else if (strtolower(substr($c, 0, 9)) == '<nopangu>') {
+            $c = substr($c, 9, strlen($c) - 9 -10);
+            $result .= $c;
+            continue;
+        } else if (strtolower(substr($c, 0, 4)) == '<pre' || strtolower(substr($c, 0, 9)) == '<textarea' || strtolower(substr($c, 0, 5)) == '<code') {
+            $result .= $c;
+            continue;
+        }
+        $result .= doPangu($c);
+    }
+    return $result;
+}
+
+/**
+ * 获取描述
+ * @return bool 是否已输出
+ */
+function getDescription() {
+    global $t;
+    if (method_exists($t,'is') && $t->is("post") || $t->is("page")) {
+        if ($t->fields->description != ""){
+            echo $t->fields->description;
+        } else {
+            $t->excerpt(80, '...');
+        }
+        return true;
+    }
+    return false;
+}
+/**
+ * 获取hide信息
+ * 防止泄露
+ */
+Typecho_Plugin::factory('Widget_Abstract_Contents')->excerptEx = array('myyodux','one');
+Typecho_Plugin::factory('Widget_Abstract_Contents')->contentEx = array('myyodux','one');
+class myyodux {
+    public static function one($con,$obj,$text)
+    {
+      $text = empty($text)?$con:$text;
+      if(!$obj->is('single')){
+      $text = preg_replace("/\[hide\](.*?)\[\/hide\]/sm",'',$text);
+      }
+               return $text;
+}
+}
